e7616f227efe6886c3e27afc96ac4e3c7062c1fb
Justin Tung <JTung1104@gmail.com>
2016-05-18 15:00:35 -0400

    update

--- camera.js
+++ camera.js
@@ -9,14 +9,14 @@
         display_name: "Authorization Code",
         type: "text",
         description: "Your personal authorization code generated from <a href=\"https://home.nest.com/login/oauth2?client_id=6a45d3f5-b753-4ede-9ebb-f445d87ce088&state=" + getCSRFtoken() + "\" target=\"_blank\">here</a>."
+      },
+      {
+        name: "refresh_time",
+        display_name: "Refresh Every",
+        type: "number",
+        suffix: "seconds",
+        default_value: 10
       }
-      // {
-      //   name: "refresh_time",
-      //   display_name: "Refresh Every",
-      //   type: "number",
-      //   suffix: "seconds",
-      //   default_value: 10
-      // }
     ],
     newInstance: function (settings, newInstanceCallback, updateCallback) {
       newInstanceCallback(new nestCamera(settings, updateCallback));
@@ -62,10 +62,10 @@
     function getData () {
       if (typeof self.access_token === "undefined") {
         getAccessToken();
-      } else {
+      } else if (typeof self.ref === "undefined") {
         self.ref = new Firebase('wss://developer-api.nest.com');
-        ref.authWithCustomToken(self.access_token);
-        self.onValueChange = ref.on('value', function (snapshot) {
+        self.ref.authWithCustomToken(self.access_token);
+        self.onValueChange = self.ref.on('value', function (snapshot) {
           var data = snapshot.val();
 
           var newData = {
@@ -110,7 +110,7 @@
     };
 
     self.updateNow = function () {
-      // getData();
+      getData();
     };
 
     self.onDispose = function () {
@@ -121,6 +121,6 @@
       }
     };
 
-    // createRefreshTimer(currentSettings.refresh_time * 1000);
+    createRefreshTimer(currentSettings.refresh_time * 1000);
   };
 }());
