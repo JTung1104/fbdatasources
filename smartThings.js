bd8050936eb2bdb743c05fe04645349ea468ec0e
Justin Tung <JTung1104@gmail.com>
2016-05-25 12:10:26 -0400

    add settings

--- smartThings.js
+++ smartThings.js
@@ -3,6 +3,25 @@
     type_name: "smartThings",
     display_name: "SmartThings",
     settings: [
+      {
+        name: "authorization_code",
+        display_name: "Authorization Code",
+        type: "text",
+        description: "Your personal authorization code generated from <a href=\"https://graph.api.smartthings.com/oauth/authorize?response_type=code&client_id=4400c472-33e3-42e0-9d92-2b9e60ebc52d&scope=app&redirect_uri=www.freeboard.io\" target=\"_blank\">here</a>."
+      },
+      {
+        name: "access_token",
+        display_name: "Access Token",
+        type: "text",
+        description: "Leave this field blank."
+      },
+      {
+        name: "refresh_time",
+        display_name: "Refresh Every",
+        type: "number",
+        suffix: "seconds",
+        default_value: 10
+      }
     ],
     newInstance: function (settings, newInstanceCallback, updateCallback) {
       newInstanceCallback(new smartThingsDatasource(settings, updateCallback));
@@ -13,6 +32,9 @@
     var self = this,
         refreshTimer,
         currentSettings = settings;
+
+    function getAccessToken () {
+    }
 
     function getData () {
     }
