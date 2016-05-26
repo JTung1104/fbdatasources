84b4cae235060f2a79f3a1fc1e6f4a3a37ddddab
Justin Tung <JTung1104@gmail.com>
2016-05-25 21:00:45 -0400

    fill out getAccessToken

--- smartThings.js
+++ smartThings.js
@@ -34,11 +34,34 @@
         currentSettings = settings;
 
     function getAccessToken () {
+      $.ajax({
+        type: "POST",
+        url: "https://graph.api.smartthings.com/oauth/token",
+        data: {
+          grant_type: "authorization_code",
+          code: currentSettings.authorization_code,
+          client_id: "4400c472-33e3-42e0-9d92-2b9e60ebc52d",
+          client_secret: "3a5114d5-b567-48c0-a3d1-af4ae2856671",
+          redirect_uri: "www.freeboard.io"
+        },
+        headers: {
+          "Content-Type": "application/x-www-form-urlencoded"
+        },
+        success: function (payload) {
+          console.log(payload);
+          currentSettings.access_token = payload.access_token;
+          getData();
+        }
+      });
     }
 
     function getData () {
     }
 
+    function getEndpoints () {
+
+    }
+    
     function createRefreshTimer (interval) {
       if (refreshTimer) {
         clearInterval(refreshTimer);
