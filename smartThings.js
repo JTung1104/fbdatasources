dee0a0df44719083c3538e82d77a6b5c1be3487b
Justin Tung <JTung1104@gmail.com>
2016-06-15 19:00:14 -0400

    add access token field

--- smartThings.js
+++ smartThings.js
@@ -10,6 +10,12 @@
         description: "Your personal authorization code generated from <a href=\"https://graph.api.smartthings.com/oauth/authorize?response_type=code&client_id=4400c472-33e3-42e0-9d92-2b9e60ebc52d&scope=app&redirect_uri=www.freeboard.io\" target=\"_blank\">here</a>."
       },
       {
+        name: "access_token",
+        display_name: "Access Token",
+        type: "text",
+        description: "Leave this field blank."
+      },
+      {
         name: "refresh_time",
         display_name: "Refresh Every",
         type: "number",
