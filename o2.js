9ae5bc9386a1fb305213978cb29c15449c4bc05b
Justin Tung <JTung1104@gmail.com>
2016-09-06 18:33:24 -0400

    update format report functions

--- o2.js
+++ o2.js
@@ -2,6 +2,9 @@
   freeboard.loadDatasourcePlugin({
     type_name: "o2",
     display_name: "O2 Concepts",
+    external_scripts: [
+      "https://cdn.rawgit.com/lightswitch05/table-to-json/master/lib/jquery.tabletojson.min.js"
+    ],
     settings: [
       {
         name: "serial_number",
@@ -26,13 +29,23 @@
         refreshTimer,
         currentSettings = settings;
 
+    var newData = {
+      "Historical": {},
+      "RDM": {
+        "Software Versions": {},
+        "Inlet Side Battery": {},
+        "Exhaust Side Battery": {}
+      }
+    };
+
     var getData = function () {
       $.ajax({
         type: "GET",
         url: "https://corsanywhere.herokuapp.com/http://www.o2pocdata.com/rdm-data/" + currentSettings.serial_number + "/refresh",
         success: function (payload) {
-          console.log(formatData(payload));
-          updateCallback(formatData(payload));
+          getReports();
+          formatRDMData(payload);
+          updateCallback(newData);
         },
         error: function (e) {
           console.log("Error", e);
@@ -43,7 +56,55 @@
       });
     };
 
-    var formatData = function (string) {
+    var getReports = function () {
+      var url = "https://cors-anywhere.herokuapp.com/http://www.o2pocdata.com/reports/?fields=SN&SN=" + currentSettings.serial_number + "&fields=MEID&MEID=&fields=date&fields=TimStrt&fields=MinsLong&fields=SWV&fields=HWV&fields=CellSS&fields=MtrRPM&fields=MtrAmps&fields=MtrTmp&fields=ContSet&fields=PulsSet&fields=MeasFlow&fields=UnitTmp&fields=O2Tmp&fields=TankPrs&fields=HosePrs&fields=BaroPrs&fields=Pur&fields=VIn&fields=CarVIn&fields=BattPer&fields=BattMa&fields=RunTime&fields=O2PurSmpl&fields=Pwrs&fields=CellRst&fields=Err&fields=ACBPM&fields=CarBPM&fields=BatBPM&fields=Batt&fields=Alrm&fields=Loc&fields=CellLoc&fields=Mode&fields=Cks&limit=0"
+
+      $.ajax({
+        type: "GET",
+        url: url,
+        success: function (payload) {
+          var table = $.parseHTML(payload)[7];
+          table.setAttribute("class", "o2-reports");
+          $("#board-content").append(table);
+          var reports = $(".o2-reports").tableToJSON({
+            allowHTML: true
+          });
+          $(".o2-reports").remove();
+
+          formatReports(reports);
+          console.log(newData);
+          updateCallback(newData);
+        },
+        error: function (e) {
+          console.log("Error", e);
+        },
+        headers: {
+          'X-Requested-With': 'XMLHttpRequest'
+        }
+      });
+    };
+
+    var getDate = function (string) {
+      var date = new Date(string).toLocaleString();
+
+      if (date === "Invalid Date") {
+        date = string.split(" ");
+        var calendarPart = date[0].split("-");
+        var timePart = date[1].split(":");
+        var year = calendarPart[0];
+        var month = calendarPart[1] - 1;
+        var day = calendarPart[2];
+        var hours = timePart[0];
+        var minutes = timePart[1];
+        var seconds = timePart[2].split(".")[0];
+
+        return new Date(year, month, day, hours, minutes, seconds).toLocaleString();
+      }
+
+      return date;
+    };
+
+    var formatRDMData = function (string) {
       var words = [];
 
       string.split(" ").forEach(function (word) {
@@ -52,66 +113,163 @@
 
       if (words.length !== 27) {return ({})} // If no data
 
-      var newData = {
-        "Software Versions": {},
-        "Inlet Side Battery": {},
-        "Exhaust Side Battery": {}
-      };
-
-      newData["Serial Number"] = words[0].slice(3);
-      newData["Last Updated"] = words[1] + " " + words[2].slice(0, -4);
-      newData["Software Versions"]["Display PCB"] = words[3].slice(0, -1);
-      newData["Software Versions"]["Main PCB"] = words[4].slice(0, -1);
+      newData["RDM"]["Serial Number"] = words[0].slice(3);
+      newData["RDM"]["Last Updated"] = words[1] + " " + words[2].slice(0, -4);
+      newData["RDM"]["Software Versions"]["Display PCB"] = words[3].slice(0, -1);
+      newData["RDM"]["Software Versions"]["Main PCB"] = words[4].slice(0, -1);
 
       var versions = words[5].split("/");
 
-      newData["Software Versions"]["Motor"] = versions[0];
-      newData["Software Versions"]["Purity Sensor"] = versions[1];
-      newData["Software Versions"]["Main PCB Revision"] = versions[2];
-      newData["Software Versions"]["Display PCB Revision"] = versions[3].slice(0, versions[3].indexOf("D"));
+      newData["RDM"]["Software Versions"]["Motor"] = versions[0];
+      newData["RDM"]["Software Versions"]["Purity Sensor"] = versions[1];
+      newData["RDM"]["Software Versions"]["Main PCB Revision"] = versions[2];
+      newData["RDM"]["Software Versions"]["Display PCB Revision"] = versions[3].slice(0, versions[3].indexOf("D"));
 
-      newData["DNA Data Records"] = versions[3].slice(versions[3].indexOf(":") + 1);
-      newData["DNA Failed Send Attempts"] = versions[4].slice(0, versions[4].indexOf(":"));
-      newData["DNA Last Attempt Status Code"] = versions[4].slice(versions[4].indexOf(":") + 1);
+      newData["RDM"]["DNA Data Records"] = versions[3].slice(versions[3].indexOf(":") + 1);
+      newData["RDM"]["DNA Failed Send Attempts"] = versions[4].slice(0, versions[4].indexOf(":"));
+      newData["RDM"]["DNA Last Attempt Status Code"] = versions[4].slice(versions[4].indexOf(":") + 1);
 
       var diagnostics = words[6].split("/");
 
-      newData["Remote Diagnostics Records"] = diagnostics[0].slice(diagnostics[0].indexOf(":") + 1);
-      newData["Remote Diagnostics Failed Send Attempts"] = diagnostics[1].slice(0, diagnostics[1].indexOf(":"));
-      newData["Remote Diagnostics Last Attempt Status Code"] = diagnostics[1].slice(diagnostics[1].indexOf(":") + 1);
+      newData["RDM"]["Remote Diagnostics Records"] = diagnostics[0].slice(diagnostics[0].indexOf(":") + 1);
+      newData["RDM"]["Remote Diagnostics Failed Send Attempts"] = diagnostics[1].slice(0, diagnostics[1].indexOf(":"));
+      newData["RDM"]["Remote Diagnostics Last Attempt Status Code"] = diagnostics[1].slice(diagnostics[1].indexOf(":") + 1);
 
-      newData["Cell Signal Strength"] = words[7].slice(3, -1);
+      newData["RDM"]["Cell Signal Strength"] = words[7].slice(3, -1);
 
-      newData["Inlet Side Battery"]["Temperature (Celsius)"] = words[8].slice(3);
-      newData["Exhaust Side Battery"]["Temperature (Celsius)"] = words[9].slice(3);
-      newData["Pump Temperature (Celsius)"] = words[10].slice(2);
-      newData["Display PCB Temperature (Celsius)"] = words[11].slice(2);
-      newData["Main PCB Temperature (Celsius)"] = words[12].slice(2);
-      newData["Oxygen Temperature (Celsius)"] = words[13].slice(3, words[13].indexOf("B"));
+      newData["RDM"]["Inlet Side Battery"]["Temperature (Celsius)"] = words[8].slice(3);
+      newData["RDM"]["Exhaust Side Battery"]["Temperature (Celsius)"] = words[9].slice(3);
+      newData["RDM"]["Pump Temperature (Celsius)"] = words[10].slice(2);
+      newData["RDM"]["Display PCB Temperature (Celsius)"] = words[11].slice(2);
+      newData["RDM"]["Main PCB Temperature (Celsius)"] = words[12].slice(2);
+      newData["RDM"]["Oxygen Temperature (Celsius)"] = words[13].slice(3, words[13].indexOf("B"));
 
-      newData["Inlet Side Battery"]["Current (milliamps)"] = words[13].slice(words[13].indexOf("B") + 3);
-      newData["Inlet Side Battery"]["Charge (%)"] = words[14];
-      newData["Inlet Side Battery"]["Minutes Until Out Of Battery"] = words[15];
-      newData["Inlet Side Battery"]["Lifetime Battery Discharge Cycles"] = words[16].slice(0, words[16].indexOf("B"));
+      newData["RDM"]["Inlet Side Battery"]["Current (milliamps)"] = words[13].slice(words[13].indexOf("B") + 3);
+      newData["RDM"]["Inlet Side Battery"]["Charge (%)"] = words[14];
+      newData["RDM"]["Inlet Side Battery"]["Minutes Until Out Of Battery"] = words[15];
+      newData["RDM"]["Inlet Side Battery"]["Lifetime Battery Discharge Cycles"] = words[16].slice(0, words[16].indexOf("B"));
 
-      newData["Exhaust Side Battery"]["Current (milliamps)"] = words[16].slice(words[16].indexOf(":" + 1));
-      newData["Exhaust Side Battery"]["Charge (%)"] = words[17];
-      newData["Exhaust Side Battery"]["Minutes Until Out Of Battery"] = words[18];
-      newData["Exhaust Side Battery"]["Lifetime Battery Discharge Cycles"] = words[19].slice(0, words[19].indexOf("V"));
+      newData["RDM"]["Exhaust Side Battery"]["Current (milliamps)"] = words[16].slice(words[16].indexOf(":" + 1));
+      newData["RDM"]["Exhaust Side Battery"]["Charge (%)"] = words[17];
+      newData["RDM"]["Exhaust Side Battery"]["Minutes Until Out Of Battery"] = words[18];
+      newData["RDM"]["Exhaust Side Battery"]["Lifetime Battery Discharge Cycles"] = words[19].slice(0, words[19].indexOf("V"));
 
-      newData["Voltage In (volts)"] = words[19].slice(words[19].indexOf(":") + 1);
-      newData["Voltage After Conversion (volts)"] = words[20];
-      newData["Last Alarm"] = words[21].slice(0, words[21].indexOf("PS"));
-      newData["Power Supply"] = (words[21].slice(-1) === "1"
+      newData["RDM"]["Voltage In (volts)"] = words[19].slice(words[19].indexOf(":") + 1);
+      newData["RDM"]["Voltage After Conversion (volts)"] = words[20];
+      newData["RDM"]["Last Alarm"] = words[21].slice(0, words[21].indexOf("PS"));
+      newData["RDM"]["Power Supply"] = (words[21].slice(-1) === "1"
                               ? "220 Watt Large Power Supply"
                               : "150 Watt Standard Power Supply");
 
-      newData["Cellular Connection"] = words[22].slice(words[22].indexOf(":") + 1) + " " + words[23].slice(0, words[23].indexOf("Flow"));
-      newData["Flow (liters per minute)"] = words[24];
-      newData["% Oxygen"] = words[25].slice(words[25].indexOf(":") + 1);
-      newData["Atmospheric Pressure (pounds per square inch)"] = words[26].slice(words[26].indexOf(":") + 1);
+      newData["RDM"]["Cellular Connection"] = words[22].slice(words[22].indexOf(":") + 1) + " " + words[23].slice(0, words[23].indexOf("Flow"));
+      newData["RDM"]["Flow (liters per minute)"] = words[24];
+      newData["RDM"]["% Oxygen"] = words[25].slice(words[25].indexOf(":") + 1);
+      newData["RDM"]["Atmospheric Pressure (pounds per square inch)"] = words[26].slice(words[26].indexOf(":") + 1);
+    };
 
-      return newData;
+    var formatReports = function (array) {
+      array.forEach(function (report) {
+        replaceLineBreaksInReport(report);
+        formatReportAlarms(report);
+        formatReportCellLoc(report);
+        formatReportLoc(report);
+        formatReportMode(report);
+        formatReportRunTime(report);
+        formatReportBatt(report);
+        report.date = getDate(report.date);
+        report.TimStrt = getDate(report.TimStrt);
+        newData["Historical"][report.date] = report;
+      });
+    };
+
+    var formatReportAlarms = function (report) {
+      if (report.Alrm) {
+        var data = report.Alrm.split("<td>");
+        report.Alrm = {};
+        for (var i = 1; i < data.length; i += 2) {
+          report.Alrm.Time = getDate(data[i].slice(0, data[i].indexOf("</td>")));
+          report.Alrm.Fault = data[i + 1].slice(0, data[i + 1].indexOf("</td>"));
+        }
+      }
+    };
+
+    var formatReportCellLoc = function (report) {
+      if (report.CellLoc) {
+        var data = report.CellLoc.split("<td>");
+        report.CellLoc = {};
+        report.CellLoc.Time = getDate(data[1].slice(0, data[1].indexOf("</td>")));
+        report.CellLoc.Latitude = data[2].slice(0, data[2].indexOf("</td>"));
+        report.CellLoc.Longitude = data[3].slice(0, data[3].indexOf("</td>"));
+        report.CellLoc.Altitude = data[4].slice(0, data[4].indexOf("</td>"));
+        report.CellLoc.Accuracy = data[5].slice(0, data[5].indexOf("</td>"));
+      }
+    };
+
+    var formatReportLoc = function (report) {
+      if (report.Loc) {
+        var data = report.Loc.split("<td>");
+        report.Loc = {};
+
+        for (var i = 1; i < data.length; i += 4) {
+          report.Loc.Time = getDate(data[i].slice(0, data[i].indexOf("</td>")));
+          report.Loc.Latitude = data[i + 1].slice(0, data[i + 1].indexOf("</td>"));
+          report.Loc.Longitude = data[i + 2].slice(0, data[i + 2].indexOf("</td>"));
+          report.Loc.Altitude = data[i + 3].slice(0, data[i + 3].indexOf("</td>"));
+        }
+      }
+    };
+
+    var formatReportMode = function (report) {
+      if (report.Mode) {
+        var data = report.Mode.split("<td>");
+        report.Mode = {};
+
+        for (var i = 1; i < data.length; i += 4) {
+          var date = getDate(data[i].slice(0, data[i].indexOf("</td>")));
+          report.Mode[date] = {};
+          report.Mode[date].Time = date;
+          report.Mode[date].Power = data[i + 1].slice(0, data[i + 1].indexOf("</td>"));
+          report.Mode[date].Flow = data[i + 2].slice(0, data[i + 2].indexOf("</td>"));
+          report.Mode[date].Set = data[i + 3].slice(0, data[i + 3].indexOf("</td>"));
+        }
+      }
+    };
+
+    var formatReportRunTime = function (report) {
+      if (report.RunTime) {
+        var data = report.RunTime.split("<td>");
+        report.RunTime = {};
+        report.RunTime.Life = data[1].slice(0, data[1].indexOf("</td>"));
+        report.RunTime.Car = data[2].slice(0, data[2].indexOf("</td>"));
+        report.RunTime.Battery = data[3].slice(0, data[3].indexOf("</td>"));
+        report.RunTime["24V"] = data[4].slice(0, data[4].indexOf("</td>"));
+        report.RunTime.Charge = data[5].slice(0, data[5].indexOf("</td>"));
+      }
+    };
+
+    var formatReportBatt = function (report) {
+      if (report.Batt) {
+        var data = report.Batt.split("<td>");
+        report.Batt = {};
+
+        for (var i = 1; i < data.length; i += 3) {
+          var SN = data[i].slice(0, data[i].indexOf("</td>"));
+          report.Batt[SN] = {};
+          report.Batt[SN].SN = SN;
+          report.Batt[SN].Cap = data[i + 1].slice(0, data[i + 1].indexOf("</td>"));
+          report.Batt[SN].RunMins = data[i + 2].slice(0, data[i + 2].indexOf("</td>"));
+        }
+      }
+    };
+
+    var replaceLineBreaksInReport = function (object) {
+      Object.keys(object).forEach(function (key) {
+        object[key] = replaceLineBreaks(object[key]);
+      });
+    };
+
+    var replaceLineBreaks = function (string) {
+      return string.replace(/(\r\n|\n|\r)/gm, " ");
     };
 
     function createRefreshTimer (interval) {
