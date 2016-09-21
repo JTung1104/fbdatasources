0d815fa9fb03458ea48f5ef11ac314d70f779182
Justin Tung <JTung1104@gmail.com>
2016-09-21 14:29:41 -0400

    add phoneDatasource.js

---
+++ phoneDatasource.js
@@ -0,0 +1,38 @@
+(function () {
+	var phoneDatasource = function (settings, updateCallback) {
+		var self = this;
+		var currentSettings = settings;
+
+    var getData = function () {
+    };
+
+		self.updateNow = function () {
+		};
+
+		self.onDispose = function () {
+		};
+
+		self.onSettingsChanged = function (newSettings) {
+			currentSettings = newSettings;
+		};
+	};
+
+	freeboard.loadDatasourcePlugin({
+		"type_name": "phone",
+		"display_name": "Phone Datasource",
+		"external_scripts": [
+			//"https://dweet.io/client/dweet.io.min.js"
+		],
+		"settings": [
+			{
+				name: "phone_number",
+				display_name: "Phone Number",
+				"description": "Please include country code and remove all symbols e.g. 12223334444",
+				type: "text"
+			}
+		],
+		newInstance: function (settings, newInstanceCallback, updateCallback) {
+			newInstanceCallback(new phoneDatasource(settings, updateCallback));
+		}
+	});
+}());
